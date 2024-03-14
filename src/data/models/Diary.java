91ff751dc4ecb8b31a879b97f23e8578cc376522
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-14 09:09:53 +0100

    codes

--- src/data/models/Diary.java
+++ src/data/models/Diary.java
@@ -1,4 +1,8 @@
 package data.models;
 
 public class Diary{
+    private final String username;
+    public Diary(String username){
+        this.username = username;
+    }
 }
--- src/test/DiaryDerosiroryTest.java
+++ src/test/DiaryDerosiroryTest.java
@@ -14,4 +14,19 @@ public class DiaryDerosiroryTest{
         diaryImp.save(diary);
         assertEquals(1,diaryImp.count());
     }
+    @Test
+    public void addTwowDiary_testTwoISAdded(){
+        DiaryImp diaryImp = new DiaryImp();
+        Diary diary = new Diary();
+        diaryImp.save(diary);
+        diaryImp.save(diary);
+        assertEquals(2,diaryImp.count());
+    }
+    @Test
+    public void findDiaryUserName_diaryIsGotten(){
+        DiaryImp diaryImp = new DiaryImp();
+        Diary diary = new Diary("username");
+        diaryImp.save(diary);
+        assertEquals(1,diaryImp.count());
+    }
 }
