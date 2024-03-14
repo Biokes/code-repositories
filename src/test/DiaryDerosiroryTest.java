b09702fa8fcb7e9313437b32c6b8857fa4766840
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-14 08:56:55 +0100

    codes

--- src/test/DiaryDerosiroryTest.java
+++ src/test/DiaryDerosiroryTest.java
@@ -4,12 +4,14 @@ import data.models.Diary;
 import data.repositories.DiaryImp;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class DiaryDerosiroryTest{
     @Test
     public void addOneDiary_testOneISAdded(){
         DiaryImp diaryImp = new DiaryImp();
         Diary diary = new Diary();
         diaryImp.save(diary);
-
+        assertEquals(1,diaryImp.count());
     }
 }
