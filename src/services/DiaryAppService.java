5354c1e4b40357d3829bd689b907f0e15615bf95
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-19 05:32:19 +0100

    codes

--- src/services/DiaryAppService.java
+++ src/services/DiaryAppService.java
@@ -4,10 +4,9 @@ import data.models.Diary;
 import data.repositories.DiaryImp;
 import data.repositories.DiaryRepository;
 import dtos.CreateDiaryRequest;
-import dtos.DiaryLoginRequest;
 
 public class DiaryAppService implements DiaryServices{
-    private DiaryRepository diaryRepository = new DiaryImp();
+    private final DiaryRepository diaryRepository = new DiaryImp();
     @Override
     public Diary createDiary(CreateDiaryRequest loginRequest){
         Diary diary = new Diary();
