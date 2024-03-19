eb974b13bebfd9ef1ad519c9333e01f014bb684c
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-19 04:12:24 +0100

    codes

--- src/data/models/Entry.java
+++ src/data/models/Entry.java
@@ -1,11 +1,48 @@
 package data.models;
 
+import java.time.LocalDate;
+
 public class Entry{
+    private String author;
+    private String body;
     private int id;
-    public Entry(int id){
-        this.id = id;
+
+    public String getAuthor(){
+        return author;
     }
+
+    public void setAuthor(String author){
+        this.author=author;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+    public void setBody(String body){
+        this.body=body;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    private String title;
+    private LocalDate date = LocalDate.now();
+
     public int getId(){
         return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
     }
 }
--- test/DiaryRepoTest/EntryImplementsTest.java
+++ test/DiaryRepoTest/EntryImplementsTest.java
@@ -1,5 +1,4 @@
 package DiaryRepoTest;
-
 import data.models.Entry;
 import data.repositories.EntryImplements;
 import data.repositories.EntryRepository;
@@ -7,81 +6,39 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
-
 public class EntryImplementsTest{
     private EntryRepository entries= new EntryImplements();
     @BeforeEach
     public void initialize(){
         entries= new EntryImplements();
     }
-@Test
-    public void testCreateEntrySaveEntry_EntryIsSaved(){
-    Entry entry1 = new Entry(2);
-    entries.save(entry1);
-    entries.save(entry1);
-    assertEquals(2, entries.count());
-}
-@Test
-    void createMulitipleEntriesThenDeleteEntry_testEntryIsDeleted(){
-    Entry entry1 = new Entry(1);
-    entries.save(entry1);
-    entries.save(entry1);
-    assertEquals(2, entries.count());
-    entries.delete(entry1);
-    assertEquals(1, entries.count());
-}
-@Test
-    void test_AddEntryDeleteEntry_EntryIsAddedAndDeleted(){
-        Entry entry1 = new Entry(2);
-        Entry entry2 = new Entry(1);
-        entries.save(entry1);
-        entries.save(entry2);
-        entries.save(entry1);
-        entries.save(entry2);
-        entries.delete(entry1);
-        entries.delete(entry1);
-        entries.delete(entry2);
+    @Test
+    void createEntry_testEntryIsCreated(){
+        entries.save(new Entry());
         assertEquals(1, entries.count());
-        entries.delete(entry2);
-        assertEquals(0, entries.count());
     }
     @Test
-    void test_saveEntry_entryIsSaved(){
-        Entry entry1 = new Entry(3);
-        for(int number = 0; number< 5; number++)
-            entries.save(entry1);
-        assertEquals(5, entries.count());
+    void deleteEntry_testEntryIsDeleted(){
+        Entry entry = new Entry();
+        entries.save(entry);
+        assertEquals (1, entries.count());
+        entries.delete(entry);
+        assertEquals (0, entries.count());
+    }
+    @Test
+    void findAllEntry_testEntryISFound(){
+        entries.save(new Entry());
+        entries.save(new Entry());
+        entries.save(new Entry());
+        assertEquals(3, entries.findAll().size());
     }
     @Test
     void findEntryById_testEntryIsFound(){
-        Entry entry1 = new Entry(12);
-        entries.save(entry1);
-        assertEquals(1, entries.count());
-        assertEquals(entry1, entries.findById(12));
-
-    }
-    @Test
-    void deleteEntryByID_entryIsDeleted(){
-        Entry entry = new Entry(1);
+        Entry entry = new Entry();
+        entry.setId(1);
+        entry.setAuthor("Author");
         entries.save(entry);
-        Entry entryFound = entries.findById(1);
-        assertEquals(entryFound,entry);
-    }
-    @Test
-    void findEntryByIdDeleteEntry_deleteryEntryFound(){
-        Entry entry = new Entry(1);
         entries.save(entry);
-        Entry entryFound = entries.findById(1);
-        assertEquals(entryFound,entry);
-        entries.delete(1);
-        assertEquals(0, entries.count());
-    }
-    @Test
-    void test_findAll_AllEntryIsGotten(){
-        Entry entry = new Entry(1);
-        entries.save(entry);
-        assertTrue(entries.count()==1);
-        assertEquals(1, entries.findAll().size());
+        assertEquals("Author",entries.findById(1).getAuthor());
     }
 }
