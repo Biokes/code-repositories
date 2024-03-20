edeb082a5ce9a03fbd77e61fc76a3300b8e19ee2
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-20 18:18:08 +0100

    commit

--- src/services/DiaryServiceImpo.java
+++ src/services/DiaryServiceImpo.java
@@ -4,9 +4,11 @@ import data.models.Diary;
 import data.repositories.DiaryImp;
 import data.repositories.DiaryRepository;
 import dtos.RegisterDiary;
+import exceptions.UserAlreadyExistException;
 
 public class DiaryServiceImpo implements DiaryServices{
     public void createDiary(RegisterDiary request){
+        validate(request);
         Diary diary = new Diary();
         diary.setUsername(request.getUserName());
         diary.setPassword(request.getPassword( ));
@@ -18,6 +20,13 @@ public class DiaryServiceImpo implements DiaryServices{
 
     public Diary findDiary(String userName){
         return diaryRepository.findDiary(userName);
+    }
+    private void validate(RegisterDiary request){
+        if(findDiary(request.getUserName( )) != null)
+            throw new UserAlreadyExistException();
+    }
+    private void validateFields(RegisterDiary request){
+
     }
     private final DiaryRepository diaryRepository = new DiaryImp();
 }
