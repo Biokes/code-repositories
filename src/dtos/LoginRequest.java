edd3afe703887798692b0fe9b92f15a9d53e6c16
Biokes <106444765+Biokes@users.noreply.github.com>
2024-03-20 19:36:29 +0100

    commit message

--- src/dtos/LoginRequest.java
+++ src/dtos/LoginRequest.java
@@ -1,4 +1,28 @@
 package dtos;
 
 public class LoginRequest{
+    private String userName;
+    private String password;
+    public LoginRequest(String userName, String password){
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setUserName(String userName){
+        this.userName=userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public LoginRequest(){}
 }
